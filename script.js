83cb5cf9dc049056610b69e7c55aad96040f1b36
Your Name <you@example.com>
2025-01-29 22:50:08 +0500

    fixing API errors, remained last add comment to input

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 "use strict";
 const URL_API = `https://wedev-api.sky.pro/api/v1/comment-feed/comments`;
 let commentsfromAPI = [];
+let saveComment = "";
+let saveName = "";
+
 
 const contentEl = document.getElementById("content");
 const addButtonEl = document.getElementById("addButton");
 const nameInputEl = document.getElementById("nameInput");
 const commentInputEl = document.getElementById("commentInput");
 const commentListEl = document.getElementById("commentList");
-
+saveComment = commentInputEl.value;
+saveName = nameInputEl.value;
 // функция времени
 const funcDate = () => {
   const currentDate = new Date();
@@ -18,6 +22,15 @@ const funcDate = () => {
   };
   return currentDate.toLocaleDateString("ru-RU", options);
 };
+
+//функция имитации загрузки API для лайков
+function delay(interval = 300) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, interval);
+  });
+}
 
 nameInputEl.addEventListener("input", () => {
   addButtonEl.disabled = false;
@@ -33,26 +46,31 @@ const initLikeBtnListener = () => {
     let id = Number(likeButtonEl.dataset.id);
     likeButtonEl.addEventListener("click", (e) => {
       e.preventDefault();
-      comments.map((comment) => {
-        if (comment.id === id) {
-          if (comment.isLiked) {
-            --comment.likes;
-            comment.isLiked = false;
-            console.log(`disLikedd comment, id:${comment.id}`);
-          } else {
-            ++comment.likes;
-            comment.isLiked = true;
-            console.log(`liked comment, id:${comment.id}`);
+      document.getElementById("likeBtn").classList.add("loading-like");
+      commentsfromAPI.map((comment) => {
+        delay(1000).then(() => {
+          if (comment.id === id) {
+            if (comment.isLiked) {
+              --comment.likes;
+              comment.isLiked = false;
+              console.log(`disLikedd comment, id:${comment.id}`);
+            } else {
+              ++comment.likes;
+              comment.isLiked = true;
+              console.log(`liked comment, id:${comment.id}`);
+            }
           }
-        }
+        });
       });
-      renderComments();
+      fetchAndRenderComments();
     });
   }
 };
 
 // Отрисовка комментариев через JS массив
 const renderComments = () => {
+  nameInputEl.value = saveName;
+  commentInputEl.value = saveComment;
   const commentsHtml = commentsfromAPI
     .map((comment) => {
       return `<li class="comment">
@@ -67,7 +85,7 @@ const renderComments = () => {
     
             <div class="likes">
               <span class="likes-counter">${comment.likes}</span>
-              <button class="like-button ${
+              <button id="likeBtn" class="like-button ${
                 comment.isLiked ? "-active-like" : ""
               }" data-id="${comment.id}" ></button>
             </div>
@@ -80,30 +98,50 @@ const renderComments = () => {
   console.log("render page");
 };
 
-fetch(URL_API, {
-  method: "GET",
-})
-  .then((res) => {
-    return res.json();
+const fetchAndRenderComments = () => {
+  fetch(URL_API, {
+    method: "GET",
   })
-  .then((resData) => {
-    commentsfromAPI = resData.comments;
-    document.getElementById("addForm").classList.add("add-form");
-    document.getElementById("loadish").classList.add("disable");
-    return renderComments();
-  });
+    .then((res) => {
+      if (res.status === 500) {
+        alert("Сервер не отвечает");
+      } else {
+        return res.json();
+      }
+    })
+    .then((res) => {
+      if (res.status === 500) {
+        alert("Сервер не отвечает");
+      } else {
+        return res;
+      }
+    })
+    .then((resData) => {
+      commentsfromAPI = resData.comments;
+      document.getElementById("addForm").classList.add("add-form");
+      document.getElementById("loadish").classList.add("disable");
+      return renderComments();
+    })
+    .catch((error) => {
+      alert("Пропал интернет соединение");
+      console.log(error);
+    });
+};
+
+fetchAndRenderComments();
 
 // Событие клика на кнопку "Написать"
 addButtonEl.addEventListener("click", (e) => {
   e.preventDefault();
-  document.getElementById("addForm").classList.remove("add-form");
-  document.getElementById("loadish").classList.remove("disable");
   if (nameInputEl.value === "") {
     alert("Введите имя");
   } else {
     if (commentInputEl.value === "") {
       alert("Введите комментарий");
     } else {
+      document.getElementById("addForm").classList.remove("add-form");
+      document.getElementById("loadish").classList.remove("disable");
+    
       fetch(URL_API, {
         method: "POST",
         body: JSON.stringify({
@@ -113,29 +151,35 @@ addButtonEl.addEventListener("click", (e) => {
           isLiked: false,
           date: funcDate(),
           likes: 0,
+          forceError: true,
         }),
       })
         .then((res) => {
-          return res.json();
+          if (res.status === 500) {
+            alert("Сервер не отвечает");
+          }
+          if (res.status === 400) {
+            alert("Имя или комментарий должен быть больше 2-х символов");
+          } else {
+            saveComment = "";
+            saveName = "";
+            return res.json();
+          }
         })
         .then(() => {
-          return fetch(URL_API, {
-            method: "GET",
-          });
+          fetchAndRenderComments();
+          nameInputEl.value = "";
+          commentInputEl.value = "";
+          console.log('added new comment clicked "Написать"');
         })
-        .then((res) => {
-          return res.json();
-        })
-        .then((resData) => {
+        .catch((error) => {
           document.getElementById("addForm").classList.add("add-form");
           document.getElementById("loadish").classList.add("disable");
-          commentsfromAPI = resData.comments;
-          return renderComments();
-        }),
-        (nameInputEl.value = "");
-      commentInputEl.value = "";
-      console.log('added new comment clicked "Написать"');
+          console.log(error);
+          nameInputEl.value = saveName;
+          commentInputEl.value = saveComment;
+          alert("Пропал интернет соединение");
+        });
     }
   }
-  renderComments();
 });
